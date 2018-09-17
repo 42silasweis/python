5605423d75e7e7e0342e635dd52f9ba88abac4f0
Silas42 <42874505+42silasweis@users.noreply.github.com>
2018-09-17 09:26:16 -0700

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,10 @@
+#multiplication.py skw
+def multi_table(a):
+	
+	for i in range(1, 11):
+		print('{0} x {1} = {2}'.format(a, i, a*1))
+		
+
+if __name__ == '__main__':
+	a = input('Enter a number: ')
+	multi_table(float(a))
