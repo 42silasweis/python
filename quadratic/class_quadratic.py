d0bf9884ce8c0db76372cb3ec9613c8b1ecfc0da
Silas42 <42874505+42silasweis@users.noreply.github.com>
2019-01-22 08:22:20 -0800

    Add files via upload

---
+++ quadratic/class_quadratic.py
@@ -0,0 +1,38 @@
+# quadratic_equation.py skw
+import math
+class QuadraticEquation:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+    def discriminant(self):
+        return ((self.b)**2) - (4 * self.a * self.c)
+
+    def x1(self):
+        if self.discriminant() < 0:
+            istr = "imaginary"
+            return istr
+        else:
+            return (-self.b + math.sqrt(self.discriminant())) / (2 * self.a)
+
+    def x2(self):
+        if self.discriminant() < 0:
+            istr = "imaginary"
+            return istr
+        else:
+            return (-self.b - math.sqrt(self.discriminant())) / (2 * self.a)
+
+if __name__ == "__main__":
+    p1 = QuadraticEquation(1,0,-9)
+    x1 = p1.x1()
+    x2 = p1.x2()
+    print ("Discriminant = ",p1.discriminant())
+    print ("x1 = ",x1," x2 = ",x2)
+
+"""
+The line if _ _name_ _ == "_ _main_ _":
+tells the proram that the code inside this
+if statement should only be executed if The
+program is executed as a standalone program.
+it will not be executed if the program is imported as a module
+"""
